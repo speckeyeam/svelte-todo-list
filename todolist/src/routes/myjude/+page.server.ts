106e2fcf368bb974b505a91558a1bbac3ec1775b
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2022-12-01 09:05:14 -0800

    myjude

--- todolist/src/routes/myjude/+page.server.ts
+++ todolist/src/routes/myjude/+page.server.ts
@@ -2,6 +2,10 @@ import type { PageServerLoad } from './$types';
 import { sessionValid } from '$server/server';
 import { error } from '@sveltejs/kit';
 import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export const prerender = false;
 // load data from the database based on the request
 export const load: PageServerLoad = async ({ request, cookies }) => {
@@ -11,6 +15,16 @@ export const load: PageServerLoad = async ({ request, cookies }) => {
 		const valid = await sessionValid(sessionId);
 		if (valid != false) {
 			const userId = valid;
+			let user = await prisma.user.findFirst({ where: { id: userId } });
+			if (user != null){
+				return {
+					post: user.todos;
+				};
+			}
+			else{
+				throw error(403, 'FORBIDDEN');
+			}
+			
 		} else {
 			throw error(403, 'FORBIDDEN');
 		}
