ab5932fb9bdc48cacbaad4b2398612bcf316f041
speckeyeam <dicklung212@gmail.com>
2022-12-08 23:06:14 -0800

    joshua true

--- todolist/src/lib/components/task.svelte
+++ todolist/src/lib/components/task.svelte
@@ -1,6 +1,38 @@
 <script lang="ts">
-	export let data: string;
+	export let data: {};
+	let originaltask = data.task;
+	let task = data.task;
+	let update: boolean = false;
+	let deleteBtn: boolean = false;
+	const taskupdated = async () => {
+		if (originaltask != task) {
+			update = true;
+		} else {
+			update = false;
+		}
+	};
+	const toggledelete = async () => {
+		if (deleteBtn) {
+			deleteBtn = false;
+		} else {
+			deleteBtn = true;
+		}
+	};
 </script>
 
-<p>Jude is {data}</p>
-<slot></slot>
+<input
+	bind:value={task}
+	on:input={taskupdated}
+	on:focusin={toggledelete}
+	style="make it not shit"
+	on:focusout={toggledelete}
+/>
+{#if deleteBtn}
+	<button>delete</button>
+{/if}
+
+{#if update}
+	<button>update</button>
+{/if}
+<br />
+<slot />
