008e757be2f66c87de9a2adde4d7df00c2bbb609
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2022-12-09 16:08:15 -0800

    jude

--- todolist/src/lib/server/server.ts
+++ todolist/src/lib/server/server.ts
@@ -22,11 +22,11 @@ export async function sessionValid(sessionId: string) {
 	}
 }
 
-export async function getTask(id: number, userid: string) {
-	const task = await prisma.todo.findUnique({ where: { id } });
+export async function getTask(id: number, userId: string) {
+	const task = await prisma.todo.findFirst({ where: { id } });
 
 	if (task != null) {
-		if (task.userId == userid) {
+		if (task.userId == userId) {
 			return task;
 		} else {
 			return null;
---
+++ todolist/src/routes/api/editJude/+server.ts
@@ -0,0 +1,35 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { sessionValid, getTask } from '$server/server';
+
+import { PrismaClient } from '@prisma/client';
+import Task from '$components/task.svelte';
+
+const prisma = new PrismaClient();
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+	const list = await request.json();
+	let valid;
+	const sessionId = cookies.get('sessionid') as string;
+	if (sessionId != null) {
+		valid = await sessionValid(sessionId);
+	}
+	let validtask = await getTask(list.id, sessionId);
+
+	if (valid) {
+		const userId = String(valid);
+		const updatelist = await prisma.todo.update({
+			where: { id: list.id },
+			data: { task: list.task }
+		});
+		if (!updatelist) {
+			return json({ sucess: false });
+		}
+	} else {
+		return json({ notLoggedIn: true });
+	}
+
+	return json({ sucess: true });
+
+	//return json(newList.value + " test");
+};
