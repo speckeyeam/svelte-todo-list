c0b13031298b6a525fb76b0f01244a3c95929e68
speckeyeam <dicklung212@gmail.com>
2022-12-22 13:17:54 -0800

    editjude

--- todolist/src/routes/api/editJude/+server.ts
+++ todolist/src/routes/api/editJude/+server.ts
@@ -11,7 +11,6 @@ export const POST: RequestHandler = async ({ cookies, request }) => {
 	const list = await request.json();
 
 	let valid;
-	let validtask;
 	const sessionId = cookies.get('sessionid') as string;
 	console.log(list);
 	if (list.method) {
@@ -19,21 +18,40 @@ export const POST: RequestHandler = async ({ cookies, request }) => {
 			if (sessionId != null) {
 				valid = await sessionValid(sessionId);
 			}
-			let validtask = await getTask(list.id, sessionId);
+			const validtask = await getTask(list.id, sessionId);
 
-	if (valid) {
-		const userId = String(valid);
-		const updatelist = await prisma.todo.update({
-			where: { id: list.id },
-			data: { task: list.task }
-		});
-		if (!updatelist) {
-			return json({ sucess: false });
+			if (valid && validtask) {
+				const userId = String(valid);
+				const updatelist = await prisma.todo.update({
+					where: { id: list.id },
+					data: { task: list.task }
+				});
+				if (!updatelist) {
+					return json({ sucess: false });
+				}
+			} else {
+				return json({ sucess: false });
+			}
+			return json({ sucess: true });
+
+			//return json(newList.value + " test");
+		} else {
+			if (sessionId != null) {
+				valid = await sessionValid(sessionId);
+			}
+
+			if (valid) {
+				const userId = String(valid);
+				const updatelist = await prisma.todo.delete({
+					where: { id: list.id }
+				});
+				if (!updatelist) {
+					return json({ sucess: false });
+				}
+			} else {
+				return json({ sucess: false });
+			}
+			return json({ sucess: true });
 		}
-	} else {
-		return json({ sucess: false });
 	}
-	return json({ sucess: true });
-
-	//return json(newList.value + " test");
 };
