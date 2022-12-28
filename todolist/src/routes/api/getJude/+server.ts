4d37bee1b0fc67ca5464e9ce8cb5a9b19bc2f7eb
speckeyeam <dicklung212@gmail.com>
2022-12-28 00:31:51 -0800

    made the myjude page a lot better

---
+++ todolist/src/routes/api/getJude/+server.ts
@@ -0,0 +1,28 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { sessionValid, getTask } from '$server/server';
+
+import { PrismaClient } from '@prisma/client';
+import Task from '$components/task.svelte';
+import { sortUserPlugins } from 'vite';
+
+const prisma = new PrismaClient();
+
+export const POST = (async ({ cookies, request }) => {
+	const list = await request.json();
+
+	const sessionId = cookies.get('sessionid') as string;
+
+	const valid = await sessionValid(sessionId);
+	if (valid) {
+		const userId = valid;
+		const user = await prisma.user.findUnique({
+			where: { id: userId },
+			include: { todos: true }
+		});
+		if (user != null) {
+			return json({ data: user.todos });
+		}
+	}
+	return json({ error: true });
+}) satisfies RequestHandler;
--- todolist/src/routes/myjude/+page.svelte
+++ todolist/src/routes/myjude/+page.svelte
@@ -3,9 +3,58 @@
 	import Task from '$components/task.svelte';
 
 	export let data: PageData;
-</script>
+	let value: string;
+	const handleSubmit = async () => {
+    
+    fetch("/api/createJude", {
+      method: "POST",
+      body: JSON.stringify({
+value
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.sucess){
+        reloadData();
+      }
+      else if (res.notLoggedIn){
+       
+        alert("not logged in")
+      }
+      
+    })
+    .catch(() => alert('Failed to submit'))
+  }
 
-<h2>JOWSH JOWSH</h2>
+  const reloadData = async () => {
+    
+    
+    fetch("/api/getJude", {
+      method: "POST",
+      body: JSON.stringify({
+        foo: "bar"
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.data){
+        data = res;
+      }
+      else if (res.notLoggedIn){
+       
+        alert("not logged in")
+      }
+      
+    })
+    .catch(() => alert('Failed to submit'))
+  }
+</script>
+<input bind:value={value} type="text">
+<br>
+
+<button on:click={handleSubmit}>submit</button>
+
+<h2>Your tasks</h2>
 
 {#each data.data as task, i}
 	<Task data={task} />
