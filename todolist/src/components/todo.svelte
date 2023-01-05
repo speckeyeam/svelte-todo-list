fcab9f8756b17ad4c7074495f85992d54dd4702c
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-01-05 14:30:05 -0800

    todo components

---
+++ todolist/src/components/todo.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { ITodo } from '../types/todo'
+    export let todo: ITodo
+    import '../css/global.css'
+
+</script>
+
+
+<li class="todo-cn flex">
+    <div class="left-cn  flex align-center">
+        <span class="form-check-input me-3">
+            <input  type="checkbox" value="" aria-label="..." checked />
+        </span>
+        <span class:completed={todo.completed}>{todo.text}</span>
+        <!-- <s>{todo.text}</s> -->
+    </div>
+    <div class="right-cn">
+        <i class="fa-solid fa-ellipsis"></i>
+    </div>
+</li>
---
+++ todolist/src/components/todoList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { ITodo } from '../types/todo'
+    export let todos: ITodo[]
+    import Todo from "./todo.svelte"
+    import '../css/global.css'
+
+
+</script>
+
+
+<div class="todolist-cn">
+    {#each todos as todo (todo.id)}
+        <Todo {todo}/>
+    {/each}
+</div>
+
+<style>
+
+</style>
