578894e4616db29eea47c10f5040e26457d6a0b5
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-01-05 21:41:09 -0800

    login page styles

--- todolist/src/routes/login/+page.svelte
+++ todolist/src/routes/login/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import '../../styles/global.scss'
+	import '../../styles/login.scss'
+
 	import { isLoggedIn } from '$stores/stores';
 	import { goto } from '$app/navigation';
 	let email: string;
@@ -35,3 +38,21 @@
 <input bind:value={email} name="email" type="email" />
 <input bind:value={password} name="password" type="password" />
 <button on:click={logIn}>Log in</button>
+
+<div class="backgroundfilter"></div>
+<img src="https://media.istockphoto.com/id/974079490/photo/jungle-leaves-background.jpg?s=170667a&w=0&k=20&c=yAjOu8rWodR9Nqc_Lk9MMFe26v2KLAILYoDo_X6OnYo=" class="backgroundimage" alt="background">
+<div class="login-cn flex vert center">
+	<div class="login-title flex vert">
+		<h1>Login</h1>
+		<!-- <div class="line-bold"></div> -->
+	</div>
+	<button class="flex"><img src="https://img.icons8.com/color/48/null/google-logo.png"/>Continue with Google</button>
+	<button class="flex"><img src="https://img.icons8.com/ios-glyphs/30/null/mac-os.png"/>Continue with Google</button>
+	<div class="line-divider"><p>or</p>
+	</div>
+	<label for="email">Email</label>
+	<input bind:value={email} name="email" type="email" />
+	<label for="password">Password</label>
+	<input id="passwordInput" bind:value={password} name="password" type="password" />
+	<button class="flex email-btn">Continue with email</button>
+</div>
